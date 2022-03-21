20d84ec6223dd15828ab28a4651f2045a96606d6
Lecongnam <namlem4u@gmail.com>
2022-03-21 15:30:48 +0700

    Feature add and delete in Home/content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ProductService } from './service/product.service';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,12 +8,10 @@ import { ProductService } from './service/product.service';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
-  constructor(private productService: ProductService) {
-    this.productService.getProduct().subscribe( (data: any) => {
-      console.log(data);
-    })
+  
+  constructor() {
   }
+
 
   
   ngOnInit(): void {
