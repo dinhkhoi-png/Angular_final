188655a322711bc88034d3eba83843173e586397
Lecongnam <namlem4u@gmail.com>
2022-03-23 18:24:18 +0700

    Search

--- src/app/home/content/content.component.ts
+++ src/app/home/content/content.component.ts
@@ -9,20 +9,23 @@ import { Product } from "../../model/Product.model";
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
-  Products:any
+  Products: any
 
 
   constructor(private productService: ProductService) {
     this.getListProduct()
-   }
+  }
 
   getListProduct() {
-    this.productService.getAllProduct().subscribe((data: any) => {
-      this.Products = data.map((item: any) => {
-        console.log(item);
-        return item
+    this.productService.getAllProduct()
+      .subscribe((data: any) => {
+        this.productItems = data.map((item: any) => {
+          return item
+        })
+
+        this.Products = this.productItems
       })
-    })
+
   }
 
   ngOnInit(): void {
@@ -32,12 +35,36 @@ export class ContentComponent implements OnInit {
 
 
   //============ Cách Thêm và xoá item
-  id:number=0
-  add(item:any){
-    let {id, ...data} = item
+  id: number = 0
+  add(item: any) {
+    let { id, ...data } = item
     this.productService.create(data)
   }
 
 
+  productItems: any = []
+  assignItem() {
+    this.productItems = Object.assign([], this.Products)
+  }
+
+  searchString: String = ''
+  search(value: any): any {
+    let result = Object.assign([], this.Products)
+    this.searchString = value.toLowerCase()
+
+    // search Result 
+    result = result.filter((item: any) => item.product_name.toLowerCase().indexOf(this.searchString) != -1)
+
+    if (result.length != 0) {
+      console.log(result);
+
+      return this.productItems = result
+    }
+
+    if (this.searchString.trim() == '') {
+      return this.assignItem()
+    }
+    return this.assignItem()
+  }
 
 }
